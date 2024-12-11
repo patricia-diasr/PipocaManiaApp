6a9e0b8ff87664a22ccfd710eeb900400757633e
patricia-diasr <patricia.d.rodrigues345@gmail.com>
2024-12-10 21:07:09 -0300

    Status bar

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { ScrollView, StyleSheet } from "react-native";
+import { ScrollView, StyleSheet, StatusBar, Platform } from "react-native";
 import { AuthProvider } from "./context/AuthContext";
 import Menu from "./components/Menu";
 import Header from "./components/Header";
@@ -7,6 +7,11 @@ import Header from "./components/Header";
 export default function Index() {
   return (
     <>
+      <StatusBar
+        barStyle={Platform.OS === "ios" ? "dark-content" : "light-content"}
+        backgroundColor={Platform.OS === "android" ? "#17082a" : "transparent"}
+        translucent={Platform.OS === "android"}
+      />
       <AuthProvider>
         <ScrollView contentContainerStyle={styles.container}>
           <Header />
